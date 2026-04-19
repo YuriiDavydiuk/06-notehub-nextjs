a4e119780b1e05d636361e4850987df011eda5bc
Yuri <davydiuk.yurii@gmail.com>
2026-04-19 23:02:55 +0300

    fix:TaskStackProvider

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -12,6 +12,7 @@ export default function NoteDetailsClient() {
   const { data, isLoading, isError } = useQuery({
     queryKey: ['note', id],
     queryFn: () => fetchNoteById(id),
+    refetchOnMount: false,
   });
 
   if (isLoading) return <p>Loading, please wait...</p>;
